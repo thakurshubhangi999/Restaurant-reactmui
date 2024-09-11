85f77a00f9c58095338681104254e5e5156731ea
Shubhangi Thakur <thakurshubhangi999@gmail.com>
2024-09-11 15:07:09 +0530

    Restaurant website using reactjs and react ui frameworks 2024

--- src/App.js
+++ src/App.js
@@ -1,48 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Menu from "./pages/Menu";
 import Pagenotfound from "./pages/Pagenotfound";
-import CircularProgress from "@mui/material/CircularProgress"; // Import Material-UI Loader
-import Box from "@mui/material/Box"; // Import Box for centering
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulating a loading delay
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000); // 2 seconds delay
-  }, []);
-
   return (
-    <div>
-      {loading ? (
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh'
-          }}
-        >
-          <CircularProgress /> {/* Material-UI loader */}
-        </Box>
-      ) : (
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="*" element={<Pagenotfound />} />
-          </Routes>
-        </BrowserRouter>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
